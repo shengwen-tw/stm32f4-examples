0d6eed22b159c894f340ecfe16e47e8631e914a9
Shengwen1997 <l1996812@gmail.com>
2013-12-14 18:03:40 +0800

    Add the function of GPIO initialization

--- Firmware/main.c
+++ Firmware/main.c
@@ -1,4 +1,24 @@
+#include <stm32f4xx.h>
+#include <stm32f4xx_gpio.h>
+
+void init_GPIO()
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {
+		.GPIO_Pin = GPIO_Pin_15,
+		.GPIO_Mode = GPIO_Mode_OUT,
+		.GPIO_Speed = GPIO_Speed_50MHz,
+		.GPIO_OType =GPIO_OType_PP,
+		.GPIO_PuPd = GPIO_PuPd_UP
+	};
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+	GPIO_Init(GPIOD, &GPIO_InitStruct);
+}
+
 int main()
 {
-    return 0;
+	init_GPIO();
+
+	return 0;
 }
