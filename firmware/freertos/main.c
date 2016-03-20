987ec9823d453f6da94a6e73e35849ae115b7364
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-03-20 23:32:37 +0800

    Add freertos pratice

--- firmware/freertos/main.c
+++ firmware/freertos/main.c
@@ -1,13 +1,15 @@
-#include <stm32f4xx.h>
-#include <stm32f4xx_gpio.h>
+#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
 
-void delay(uint32_t count)
-{
-	while(count--);
-}
+#include "FreeRTOS.h"
+#include "task.h"
+
+#define MILLI_SECOND_TICK (configTICK_RATE_HZ / 1000)
 
 void init_GPIO()
 {
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
 	GPIO_InitTypeDef GPIO_InitStruct = {
 		.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15,
 		.GPIO_Mode = GPIO_Mode_OUT,
@@ -16,29 +18,45 @@ void init_GPIO()
 		.GPIO_PuPd = GPIO_PuPd_DOWN
 	};
 
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
 	GPIO_Init(GPIOD, &GPIO_InitStruct);
+}
+
+void task1()
+{
+	int digitStatus = 1;
+
+	while(1) {
+		GPIO_WriteBit(GPIOD, GPIO_Pin_12, digitStatus);
+		GPIO_WriteBit(GPIOD, GPIO_Pin_13, digitStatus);	
+
+		digitStatus = (digitStatus + 1) % 2;
+
+		vTaskDelay(MILLI_SECOND_TICK);
+	}
+}
+
+void task2()
+{
+	int digitStatus = 1;
+
+	while(1) {
+		GPIO_WriteBit(GPIOD, GPIO_Pin_14, digitStatus);
+		GPIO_WriteBit(GPIOD, GPIO_Pin_15, digitStatus);
+
+		digitStatus = (digitStatus + 1) % 2;
+
+		vTaskDelay(MILLI_SECOND_TICK);
+	}
 }
 
 int main()
 {
 	init_GPIO();
 
-	int digitStatus = 1;
+	xTaskCreate(task1, "task1", 256, NULL, tskIDLE_PRIORITY + 2, NULL);
+	xTaskCreate(task2, "task2", 256, NULL, tskIDLE_PRIORITY + 1, NULL);
 
-	while(1) {
-		GPIO_WriteBit(GPIOD, GPIO_Pin_12, digitStatus);
-		delay(1000000L);
-		GPIO_WriteBit(GPIOD, GPIO_Pin_13, digitStatus);	
-		delay(1000000L);
-		GPIO_WriteBit(GPIOD, GPIO_Pin_14, digitStatus);
-		delay(1000000L);
-		GPIO_WriteBit(GPIOD, GPIO_Pin_15, digitStatus);
-		delay(1000000L);
-
-		digitStatus = (digitStatus + 1) % 2;
-	}
+	vTaskStartScheduler();
 
 	return 0;
 }
