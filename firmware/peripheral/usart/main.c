b6c0336a1b3235a3f20902e43e711a1b58f7aaff
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-03-21 09:45:40 +0800

    Arrange the code

--- firmware/peripheral/usart/main.c
+++ firmware/peripheral/usart/main.c
@@ -5,15 +5,16 @@ void delay(uint32_t count)
 	while(count--);
 }
 
-
-void init_RCC()
+void usart3_init()
 {
+	/* RCC initialization */
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
-}
 
-void init_GPIO()
-{
+	/* GPIO initialization */
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
+
 	GPIO_InitTypeDef GPIO_InitStruct = {
 		.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11,
 		.GPIO_Mode = GPIO_Mode_AF,
@@ -21,15 +22,9 @@ void init_GPIO()
 		.GPIO_OType = GPIO_OType_PP,
 		.GPIO_PuPd = GPIO_PuPd_UP
 	};
-
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_USART3);
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource11, GPIO_AF_USART3);
-
 	GPIO_Init(GPIOC, &GPIO_InitStruct);
-}
 
-void init_USART()
-{
+	/* USART initialization */
 	USART_InitTypeDef USART_InitStruct = {
 		.USART_BaudRate = 9600,
 		.USART_Mode = USART_Mode_Rx | USART_Mode_Tx,
@@ -37,13 +32,12 @@ void init_USART()
 		.USART_StopBits = USART_StopBits_1,
 		.USART_Parity = USART_Parity_No
 	};
-
 	USART_Init(USART3, &USART_InitStruct);
 
 	USART_Cmd(USART3, ENABLE);
 }
 
-void send_data(char *string)
+void usart_puts(char *string)
 {
 	while(*string != '\0') {
 		USART_SendData(USART3, (uint16_t)(*string));
@@ -54,12 +48,10 @@ void send_data(char *string)
 
 int main()
 {
-	init_RCC();
-	init_GPIO();
-	init_USART();
+	usart3_init();
 
 	while(1) {
-		send_data("Hello World!\n\r");
+		usart_puts("Hello World!\n\r");
 		delay(1000000L);		
 	}
 
