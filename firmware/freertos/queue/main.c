7763451015c7955c053967a774d0cec73554543d
Shengwen Cheng <shengwen1997.tw@gmail.com>
2016-03-25 23:52:02 +0800

    Fix the queue example

--- firmware/freertos/queue/main.c
+++ firmware/freertos/queue/main.c
@@ -17,16 +17,18 @@ typedef struct {
 	char string[16];
 } queue_item_t;
 
-QueueHandle_t queue_item_queue_handle;
+QueueHandle_t queue_item_handle;
 
 /* Queue send task */
 void feed_you_task()
 {
 	while(1) {
 		queue_item_t queue_item;
-		strcpy(queue_item.string, "Hello\n");
+		strcpy(queue_item.string, "Hello\n\r");
 
-		xQueueSendToBack(queue_item_queue_handle, &queue_item, portMAX_DELAY);
+		while(xQueueSendToBack(queue_item_handle, &queue_item, portMAX_DELAY) != pdTRUE);
+
+		vTaskDelay(MILLI_SECOND_TICK * 10);
 	}
 }
 
@@ -36,7 +38,7 @@ void feed_me_task()
 	queue_item_t received_queue_item;
 
 	while(1) {
-		if(xQueueReceive(queue_item_queue_handle, &received_queue_item, portMAX_DELAY) == pdTRUE) {
+		if(xQueueReceive(queue_item_handle, &received_queue_item, portMAX_DELAY) == pdTRUE) {
 			usart3_puts(received_queue_item.string);
 		}
 	}
@@ -46,7 +48,7 @@ int main()
 {
 	usart3_init(9600);
 
-	xQueueCreate(QUEUE_SIZE, sizeof(queue_item_t));
+	queue_item_handle = xQueueCreate(QUEUE_SIZE, sizeof(queue_item_t));
 
 	xTaskCreate(feed_you_task, "feed me task", 256, NULL, tskIDLE_PRIORITY + 2, NULL);
 	xTaskCreate(feed_me_task, "feed you task", 256, NULL, tskIDLE_PRIORITY + 1, NULL);
